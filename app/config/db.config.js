45674f5e9879032b1041c2716926ab5d29ae63a1
Somesh Panigrahi <sonupanigrahi28@gmail.com>
2025-09-14 21:08:37 +0530

    Dockerfile and docker-compose setup

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,7 @@
+// use "host.docker.internal" as HOST when suppose node-js container wants to talk to a postgres instance on you host machine.
+// if both are dockerized use the postgresql container name in the HOST section.
 module.exports = {
-  HOST: "localhost",
+  HOST: "host.docker.internal", // you can use "localhost" when you are not testing via docker 
   USER: "postgres",
   PASSWORD: "123",
   DB: "testdb",
